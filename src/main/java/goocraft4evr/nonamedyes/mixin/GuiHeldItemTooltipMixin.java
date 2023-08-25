4730d52c1c3943380a4bafa6e3252c097420d1f3
Gooey Goo <yourfriendtheuniversalgooeygoo@gmail.com>
2023-08-24 23:22:11 -0400

    Fixed game crashing when trying to craft labels using Light Yellow dye or Indigo dye

--- src/main/java/goocraft4evr/nonamedyes/mixin/GuiHeldItemTooltipMixin.java
+++ src/main/java/goocraft4evr/nonamedyes/mixin/GuiHeldItemTooltipMixin.java
@@ -1,5 +1,6 @@
 package goocraft4evr.nonamedyes.mixin;
 
+import goocraft4evr.nonamedyes.NoNameDyes;
 import net.minecraft.client.gui.GuiHeldItemTooltip;
 import net.minecraft.core.item.ItemStack;
 import net.minecraft.core.util.helper.Color;
@@ -7,7 +8,7 @@ import net.minecraft.core.util.helper.Colors;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value= GuiHeldItemTooltip.class, remap = false)
@@ -15,10 +16,8 @@ public abstract class GuiHeldItemTooltipMixin {
     @Shadow
     private Color color;
 
-    @Inject(method = "updateString", at = @At("TAIL"))
-    private void injected(ItemStack item, CallbackInfo ci) {
-        if (item.tag.getBoolean("overrideColor")&&item.getNameColor()>21) {
-            this.color = Colors.allChatColors[item.getNameColor()-6];
-        }
+    @Redirect(method = "updateString", at = @At(value="INVOKE",target = "Lnet/minecraft/core/item/ItemStack;getNameColor()B"))
+    private byte injected(ItemStack item) {
+        return (byte)(item.getNameColor()-((item.tag.getBoolean("overrideColor")&&item.getNameColor()>21)?6:0));
     }
 }
