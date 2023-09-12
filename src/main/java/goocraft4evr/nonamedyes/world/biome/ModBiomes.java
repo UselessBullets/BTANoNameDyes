aef931d930bcee6e9e6db6ee45c3776eed954214
Gooey Goo <yourfriendtheuniversalgooeygoo@gmail.com>
2023-09-12 18:51:46 -0400

    Cinnamon forests now spawn naturally (need to fix spawn rates).

--- src/main/java/goocraft4evr/nonamedyes/world/biome/ModBiomes.java
+++ src/main/java/goocraft4evr/nonamedyes/world/biome/ModBiomes.java
@@ -1,12 +1,13 @@
 package goocraft4evr.nonamedyes.world.biome;
 
+import goocraft4evr.nonamedyes.NoNameDyes;
 import net.minecraft.core.world.biome.Biome;
 import net.minecraft.core.world.biome.Biomes;
 import net.minecraft.core.world.weather.Weather;
 
 public abstract class ModBiomes {
     public static final Biome OVERWORLD_CINNAMON_FOREST =
-            Biomes.register("minecraft:overworld.cinnamon_forest",
+            Biomes.register(NoNameDyes.MOD_ID+":overworld.cinnamon_forest",
                     new BiomeCinnamonForest()
                     .setColor(588342)
                     .setBlockedWeathers(Weather.overworldSnow));
