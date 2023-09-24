a0fd9b7fd533e0dc9229f909c6bd87282ab2da27
Gooey Goo <yourfriendtheuniversalgooeygoo@gmail.com>
2023-09-24 13:32:24 -0400

    Changed the spawning of ebony forests.

--- src/main/java/goocraft4evr/nonamedyes/mixin/core/world/biome/provider/BiomeProviderOverworldMixin.java
+++ src/main/java/goocraft4evr/nonamedyes/mixin/core/world/biome/provider/BiomeProviderOverworldMixin.java
@@ -16,11 +16,7 @@ public abstract class BiomeProviderOverworldMixin {
     @Shadow @Final private static BiomeRangeMap brm;
     @Inject(method="init",at=@At(value = "INVOKE",ordinal=14,target = "Lnet/minecraft/core/world/biome/data/BiomeRangeMap;addRange(Lnet/minecraft/core/world/biome/Biome;[Lnet/minecraft/core/world/biome/data/BiomeRange;)V"))
     private static void injectAfterForest(CallbackInfo ci) {
+        brm.addRange(ModBiomes.OVERWORLD_EBONY_FOREST, new BiomeRange(0.7125,0.98,0.4375,0.5725,0.0,1.0,0.0,1.0));
         brm.addRange(ModBiomes.OVERWORLD_CINNAMON_FOREST, new BiomeRange(0.9,1.0,0.7,1.0,0.0,1.0,0.0,1.0));
-    }
-
-    @Inject(method="init",at=@At(value = "INVOKE",ordinal=12,target = "Lnet/minecraft/core/world/biome/data/BiomeRangeMap;addRange(Lnet/minecraft/core/world/biome/Biome;[Lnet/minecraft/core/world/biome/data/BiomeRange;)V"))
-    private static void injectAfterPlains(CallbackInfo ci) {
-        brm.addRange(ModBiomes.OVERWORLD_EBONY_FOREST, new BiomeRange(0.55,0.9,0.4375,0.6,0.0,1.0,0.0,1.0));
     }
 }
