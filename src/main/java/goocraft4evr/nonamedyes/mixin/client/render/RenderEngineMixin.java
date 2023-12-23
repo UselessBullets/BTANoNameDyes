2b4f369230465ae8676802436b9fbe735a9d4e89
Gooey Goo <yourfriendtheuniversalgooeygoo@gmail.com>
2023-12-23 00:54:22 -0500

    modifed RenderEngineMixin

--- src/main/java/goocraft4evr/nonamedyes/mixin/client/render/RenderEngineMixin.java
+++ src/main/java/goocraft4evr/nonamedyes/mixin/client/render/RenderEngineMixin.java
@@ -4,19 +4,26 @@ import goocraft4evr.nonamedyes.NoNameDyes;
 import goocraft4evr.nonamedyes.mixin.client.render.colorizer.ColorPropertiesAccessor;
 import net.minecraft.client.render.RenderEngine;
 import net.minecraft.client.render.texturepack.TexturePack;
+import net.minecraft.client.render.texturepack.TexturePackList;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Properties;
 
 @Mixin(value= RenderEngine.class, remap = false)
 public abstract class RenderEngineMixin {
-    @Inject(method="refreshTextures",at=@At("TAIL"),locals = LocalCapture.CAPTURE_FAILHARD)
-    private void injected(CallbackInfo ci, TexturePack texturePack) {
+    @Shadow
+    public TexturePackList texturePacks;
+
+    @Inject(method="refreshTextures",at=@At("TAIL"))
+    private void injected(List<Throwable> errors, CallbackInfo ci) {
+        TexturePack texturePack = texturePacks.selectedTexturePack;
         Properties colors = new Properties();
         try {
             colors.load(texturePack.getResourceAsStream("/assets/"+ NoNameDyes.MOD_ID+"/misc/colors.properties"));
