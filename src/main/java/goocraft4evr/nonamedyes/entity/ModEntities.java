53c65786c13100e0850bf3a9bbff288f2475e50a
gooey goo <yourfriendtheuniversalgooeygoo@gmail.com>
2024-04-20 15:56:01 -0400

    Revised assets, sea snails now drop dye.

--- src/main/java/goocraft4evr/nonamedyes/entity/ModEntities.java
+++ src/main/java/goocraft4evr/nonamedyes/entity/ModEntities.java
@@ -2,6 +2,7 @@ package goocraft4evr.nonamedyes.entity;
 
 import goocraft4evr.nonamedyes.client.render.entity.SeaSnailRenderer;
 import goocraft4evr.nonamedyes.entity.animal.EntitySeaSnail;
+import goocraft4evr.nonamedyes.item.ModItems;
 import net.minecraft.client.gui.guidebook.mobs.MobInfoRegistry;
 import net.minecraft.core.item.Item;
 import net.minecraft.core.item.ItemStack;
@@ -18,7 +19,7 @@ public class ModEntities {
 
 		MobInfoRegistry.register(EntitySeaSnail.class, "sea.snail.name", "sea.snail.desc",
 			10, 400, new MobInfoRegistry.MobDrop[]{
-				new MobInfoRegistry.MobDrop(new ItemStack(Item.bucketMilk),
-					1.0f, 1, 1)});
+				new MobInfoRegistry.MobDrop(new ItemStack(ModItems.dye,1,12),
+					1.0f, 2, 5)});
 	}
 }
--- src/main/java/goocraft4evr/nonamedyes/entity/animal/EntitySeaSnail.java
+++ src/main/java/goocraft4evr/nonamedyes/entity/animal/EntitySeaSnail.java
@@ -1,22 +1,26 @@
 package goocraft4evr.nonamedyes.entity.animal;
 
 import goocraft4evr.nonamedyes.NoNameDyes;
+import goocraft4evr.nonamedyes.item.ModItems;
 import net.minecraft.core.Global;
 import net.minecraft.core.entity.Entity;
 import net.minecraft.core.entity.animal.EntityWaterAnimal;
 import net.minecraft.core.entity.player.EntityPlayer;
 import net.minecraft.core.item.Item;
+import net.minecraft.core.item.ItemStack;
 import net.minecraft.core.util.phys.AABB;
 import net.minecraft.core.world.World;
 
 import java.util.List;
 
 public class EntitySeaSnail extends EntityWaterAnimal {
+	private int timeUntilNextGoo;
 	public EntitySeaSnail(World world) {
 		super(world);
 		skinName = "seasnail";
 		setSize(0.9f, 0.9f);
 		moveSpeed = 0.1f;
+		timeUntilNextGoo = this.random.nextInt(3000) + 3000;
 	}
 
 	@Override
@@ -36,61 +40,13 @@ public class EntitySeaSnail extends EntityWaterAnimal {
 
 	@Override
 	public void onLivingUpdate() {
-		//vanilla code...
-		if (this.newPosRotationIncrements > 0) {
-			double d3;
-			double d = this.x + (this.newPosX - this.x) / (double)this.newPosRotationIncrements;
-			double d1 = this.y + (this.newPosY - this.y) / (double)this.newPosRotationIncrements;
-			double d2 = this.z + (this.newPosZ - this.z) / (double)this.newPosRotationIncrements;
-			for (d3 = this.newRotationYaw - (double)this.yRot; d3 < -180.0; d3 += 360.0) {
-			}
-			while (d3 >= 180.0) {
-				d3 -= 360.0;
-			}
-			this.yRot = (float)((double)this.yRot + d3 / (double)this.newPosRotationIncrements);
-			this.xRot = (float)((double)this.xRot + (this.newRotationPitch - (double)this.xRot) / (double)this.newPosRotationIncrements);
-			--this.newPosRotationIncrements;
-			this.setPos(d, d1, d2);
-			this.setRot(this.yRot, this.xRot);
-			List<AABB> list1 = this.world.getCubes(this, this.bb.getInsetBoundingBox(0.03125, 0.0, 0.03125));
-			if (list1.size() > 0) {
-				double d4 = 0.0;
-				for (int j = 0; j < list1.size(); ++j) {
-					AABB axisalignedbb = list1.get(j);
-					if (!(axisalignedbb.maxY > d4)) continue;
-					d4 = axisalignedbb.maxY;
-				}
-				this.setPos(d, d1 += d4 - this.bb.minY, d2);
-			}
-		}
-		if (this.isMovementBlocked()) {
-			this.isJumping = false;
-			this.moveStrafing = 0.0f;
-			this.moveForward = 0.0f;
-			this.randomYawVelocity = 0.0f;
-		} else if (!this.isMultiplayerEntity) {
-			this.updatePlayerActionState();
-		}
-		//prevent jumping in water
-		if (isJumping) {
-			if (isInLava()) {
-				yd += 0.04;
-			} else if (this.onGround) {
-				jump();
-			}
-		}
-		//vanilla code...
-		this.moveStrafing *= 0.98f;
-		this.moveForward *= 0.98f;
-		this.randomYawVelocity *= 0.9f;
-		this.moveEntityWithHeading(this.moveStrafing, this.moveForward);
-		List<Entity> list = this.world.getEntitiesWithinAABBExcludingEntity(this, this.bb.expand(0.2f, 0.0, 0.2f));
-		if (list != null && !list.isEmpty()) {
-			for (int i = 0; i < list.size(); ++i) {
-				Entity entity = list.get(i);
-				if (!entity.isPushable()) continue;
-				entity.push(this);
-			}
+		super.onLivingUpdate();
+		if (isJumping && (isInWater())) yd -= 0.04;
+
+		if (!this.world.isClientSide && --timeUntilNextGoo <= 0) {
+			this.world.playSoundAtEntity(null, this, "mob.chickenplop", 1.0f, (this.random.nextFloat() - this.random.nextFloat()) * 0.2f + 1.0f);
+			this.spawnAtLocation(new ItemStack(ModItems.dye.id,1+this.random.nextInt(3),12),0);
+			this.timeUntilNextGoo = this.random.nextInt(3000) + 3000;
 		}
 	}
 
@@ -112,12 +68,15 @@ public class EntitySeaSnail extends EntityWaterAnimal {
 	}
 
 	@Override
-	protected int getDropItemId() {
-		return Item.sulphur.id;
+	protected boolean makeStepSound() {
+		return false;
 	}
 
 	@Override
-	protected boolean makeStepSound() {
-		return false;
+	protected void dropFewItems() {
+		int j = 2+this.random.nextInt(4);
+		for (int k = 0; k < j; ++k) {
+			this.spawnAtLocation(new ItemStack(ModItems.dye.id,1,12), 1);
+		}
 	}
 }
